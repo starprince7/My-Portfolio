c409effcedcf0e94fcdc1b717e9ebcf6d8da2e39
starprince7 <princeagezinweke@gmail.com>
2020-11-11 18:45:14 +0100

    Added A modal Pop up And finished the Ajax request.

--- client/ajax.js
+++ client/ajax.js
@@ -5,9 +5,19 @@
 } */
 
 const form = document.getElementById("contact__form");
+const loader = document.querySelector(".loader");
+const modal_back = document.querySelector(".modal__background");
+const Modal = document.querySelector("#modal");
+const close_modal = document.getElementById("close-btn");
+
+close_modal.addEventListener("click", () => {
+  modal_back.style.display = "none";
+  Modal.style.display = "none";
+});
 
 form.addEventListener("submit", (e) => {
   e.preventDefault();
+  loader.style.display = "block";
   const name = form.name.value;
   const email = form.email.value;
   console.log(name, email);
@@ -23,7 +33,12 @@ form.addEventListener("submit", (e) => {
       result
         .json()
         .then((data) => {
+          loader.style.display = "none";
           console.log("Response from the backend :", data);
+          if (data) {
+            modal_back.style.display = "block";
+            Modal.style.display = "block";
+          }
         })
         .catch((err) => console.log(err));
     })
